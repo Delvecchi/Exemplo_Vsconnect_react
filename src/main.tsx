328fe8ba9262342a260121f9c6023e0e866a8464
Heitor dos Santos Delvechi <388.098.958-35@info.local>
2023-08-14 21:50:43 -0300

    Atualização filter, pesquisa

--- src/main.tsx
+++ src/main.tsx
@@ -8,6 +8,7 @@ import Home from './pages/Home'
 // import CadastroDev from './pages/CadastroDev'
 import ListaServicos from './pages/ListaServicos'
 import ListaDevs from './pages/ListaDevs'
+import CardDev from './components/CardDev'
 
 
 
@@ -21,6 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
         <Route path='listaservicos' element={<ListaServicos />} />
         {/* <Route path='cadastrodev' element={<CadastroDev/>} /> */}
         <Route path='listadevs' element={<ListaDevs/>}/>
+        <Route path='card/dev' element={<CardDev />} />
         </Routes>
         <Footer1 />
       </BrowserRouter>
